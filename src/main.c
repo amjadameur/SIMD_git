1fe9b368e729b76d5353c00763fd6cc1d71fb228
amjadameur <ouledameur.amjad@gmail.com>
2019-05-13 00:28:07 +0200

    code refactoring

--- src/main.c
+++ src/main.c
@@ -4,76 +4,8 @@
 
 
 
-void tgaRgb(tgaInfo tgaData, int x, int y, Uint8* r, Uint8* g, Uint8* b) {
-	Uint8 * ptr = tgaData.tgaIm + 3*x + 3*tgaData.tgaWidth*y;
-	*b = *ptr++;
-	*g = *ptr++;
-	*r = *ptr++;
-}
-
-void findRgb(int *textureIdx, tgaInfo tgaData, Uint8* r, Uint8* g, Uint8* b) {
-	int x = 0, y = 0;
-	int rTmp = 0, gTmp = 0, bTmp = 0;
-
-	for (int i = 0; i < 3; ++i)
-	{
-		x = tgaData.tgaWidth *ModelGetTexcoord(textureIdx[i]).x;
-		y = tgaData.tgaHeight*ModelGetTexcoord(textureIdx[i]).y;
-		tgaRgb(tgaData, x, y, r, g, b);	
-		rTmp += *r;
-		gTmp += *g;
-		bTmp += *b;
-	}
-
-	*r = (Uint8) (rTmp/3);
-	*g = (Uint8) (gTmp/3);
-	*b = (Uint8) (bTmp/3);
-}
 
 
-
-
-void subVect3f(vec3f_t* v2, vec3f_t* v1) {
-	v2->x = v2->x - v1->x;
-	v2->y = v2->y - v1->y;
-	v2->z = v2->z - v1->z;
-}
-
-vec3f_t crossProduct(vec3f_t a, vec3f_t b, vec3f_t c) {
-	vec3f_t vProduct;
-	
-	subVect3f(&b, &a);
-	subVect3f(&c, &a);
-
-	vProduct.x = c.y*b.z - c.z*b.y;
-	vProduct.y = c.z*b.x - c.x*b.z;
-	vProduct.z = c.x*b.y - c.y*b.x;
-
-	// Normalizing
-	float magX = vProduct.x*vProduct.x;
-	float magY = vProduct.y*vProduct.y;
-	float magZ = vProduct.z*vProduct.z;
-
-	float magnitude = sqrt(magX+magY+magZ);
-
-	vProduct.x = vProduct.x/magnitude;
-	vProduct.y = vProduct.y/magnitude;
-	vProduct.z = vProduct.z/magnitude; 
-
-	return vProduct;
-}
-
-float dotProduct(vec3f_t a, vec3f_t b) {
-	return a.x*b.x + a.y*b.y + a.z*b.z;
-}
-
-int maxInt2(int a, int b) {
-	return (a > b) ? a : b;
-}
-
-int maxInt3(int a, int b, int c) {
-	return maxInt2(a, maxInt2(b, c));
-}
 
 void drawObjZ(window_t* w, tgaInfo tgaData, vec3f_t luminance, chosenPlane plane) {
 	// Draw obj file
@@ -86,6 +18,7 @@ void drawObjZ(window_t* w, tgaInfo tgaData, vec3f_t luminance, chosenPlane plane
 
 	windowXYZ winXYZ;
 
+	float faceLuminance;
 	
 	//////Z buffer ////////////////////////////////////////////////////////////////
 	int** zBuffer = (int**) malloc(w->width*sizeof(int*));
@@ -94,9 +27,6 @@ void drawObjZ(window_t* w, tgaInfo tgaData, vec3f_t luminance, chosenPlane plane
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////
-
-	vec3f_t cProduct= {0, 0, 0};
-	float dProduct = 0;
 
 	for (int r = 0; r < w->width; ++r) {
 		for (int c = 0; c < w->height; ++c) {
@@ -117,13 +47,12 @@ void drawObjZ(window_t* w, tgaInfo tgaData, vec3f_t luminance, chosenPlane plane
 		
 		zMax = maxInt3(z[0], z[1], z[2]);
 
-		cProduct = crossProduct(vect3tmp[0], vect3tmp[1], vect3tmp[2]);	
-		dProduct = dotProduct(luminance, cProduct);
+		faceLuminance = isFaceEnlighted(luminance, vect3tmp);
 
-		if (dProduct>0)	{
-			dProduct =(dProduct > 1) ? 1 : dProduct;
+		if (faceLuminance>0)	{
+			faceLuminance = (faceLuminance > 1) ? 1 : faceLuminance;
 			findRgb(facetmp.vt, tgaData, &r, &g, &b);
-			WindowDrawTriangleZ(w, zMax, zBuffer, x[0], y[0], x[1], y[1], x[2], y[2], dProduct*r, dProduct*g, dProduct*b);
+			WindowDrawTriangleZ(w, zMax, zBuffer, x[0], y[0], x[1], y[1], x[2], y[2], faceLuminance*r, faceLuminance*g, faceLuminance*b);
 		}
 	}
 
--- src/main.h
+++ src/main.h
@@ -26,6 +26,14 @@ typedef struct tga_info {
 
 typedef struct window_XYZ {float x; float y; float z;} windowXYZ; 
 
+int maxInt2(int a, int b) {
+	return (a > b) ? a : b;
+}
+
+int maxInt3(int a, int b, int c) {
+	return maxInt2(a, maxInt2(b, c));
+}
+
 vec3f_t setLuminance(chosenPlane plane) {
 	vec3f_t luminance = {0, 0, 0};
 
@@ -42,6 +50,33 @@ vec3f_t setLuminance(chosenPlane plane) {
 
 	return luminance;
 }
+
+void tgaRgb(tgaInfo tgaData, int x, int y, Uint8* r, Uint8* g, Uint8* b) {
+	Uint8 * ptr = tgaData.tgaIm + 3*x + 3*tgaData.tgaWidth*y;
+	*b = *ptr++;
+	*g = *ptr++;
+	*r = *ptr++;
+}
+
+void findRgb(int *textureIdx, tgaInfo tgaData, Uint8* r, Uint8* g, Uint8* b) {
+	int x = 0, y = 0;
+	int rTmp = 0, gTmp = 0, bTmp = 0;
+
+	for (int i = 0; i < 3; ++i)
+	{
+		x = tgaData.tgaWidth *ModelGetTexcoord(textureIdx[i]).x;
+		y = tgaData.tgaHeight*ModelGetTexcoord(textureIdx[i]).y;
+		tgaRgb(tgaData, x, y, r, g, b);	
+		rTmp += *r;
+		gTmp += *g;
+		bTmp += *b;
+	}
+
+	*r = (Uint8) (rTmp/3);
+	*g = (Uint8) (gTmp/3);
+	*b = (Uint8) (bTmp/3);
+}
+
 
 windowXYZ getXYZ(window_t* w, chosenPlane plane, vec3f_t vertexVect) {
 	windowXYZ winXYZ;
@@ -73,3 +108,50 @@ void freeIntMatrix(int width, int **matrix) {
 	}
 	free(matrix);
 }
+
+
+void subVect3f(vec3f_t* v2, vec3f_t* v1) {
+	v2->x = v2->x - v1->x;
+	v2->y = v2->y - v1->y;
+	v2->z = v2->z - v1->z;
+}
+
+vec3f_t crossProduct(vec3f_t a, vec3f_t b, vec3f_t c) {
+	vec3f_t vProduct;
+	
+	subVect3f(&b, &a);
+	subVect3f(&c, &a);
+
+	vProduct.x = c.y*b.z - c.z*b.y;
+	vProduct.y = c.z*b.x - c.x*b.z;
+	vProduct.z = c.x*b.y - c.y*b.x;
+
+	// Normalizing
+	float magX = vProduct.x*vProduct.x;
+	float magY = vProduct.y*vProduct.y;
+	float magZ = vProduct.z*vProduct.z;
+
+	float magnitude = sqrt(magX+magY+magZ);
+
+	vProduct.x = vProduct.x/magnitude;
+	vProduct.y = vProduct.y/magnitude;
+	vProduct.z = vProduct.z/magnitude; 
+
+	return vProduct;
+}
+
+float dotProduct(vec3f_t a, vec3f_t b) {
+	return a.x*b.x + a.y*b.y + a.z*b.z;
+}
+
+float isFaceEnlighted(vec3f_t luminance, vec3f_t *vect3tmp) {
+
+	vec3f_t cProduct= {0, 0, 0};
+	float dProduct = 0;
+
+	cProduct = crossProduct(vect3tmp[0], vect3tmp[1], vect3tmp[2]);	
+	dProduct = dotProduct(luminance, cProduct);
+
+	return dProduct;
+}
+
